ea19946526ac9d2f22f6cca522b08c1839df0cde
Andréia de Souza Ohe <andreiadesouzaohe@Andreias-MacBook-Air.local>
2017-09-07 17:21:09 -0700

    implemented modulo de rotas

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { ModuleWithProviders } from '@Angular/core';
+import { Routes, RouterModule } from '@Angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+
+
+const appRoutes: Routes = [
+    { path: 'cursos', component: CursosComponent },
+    { path: 'curso/:id', component: CursoDetalheComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
+    { path: '', component: HomeComponent }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
