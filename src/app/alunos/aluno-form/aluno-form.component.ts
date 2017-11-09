8398ff4738d66769a2b2e7c0ba2660b69ce13a27
Andréia de Souza Ohe <andreiadesouzaohe@Andreias-MacBook-Air.local>
2017-11-08 16:20:50 -0800

    implemented guarda de rotas: CanDeactivate com interface genérica

--- src/app/alunos/aluno-form/aluno-form.component.ts
+++ src/app/alunos/aluno-form/aluno-form.component.ts
@@ -1,3 +1,4 @@
+import { IFormCanDeactivate } from './../../guards/iform-candeactivate';
 import { AlunosService } from './../alunos.service';
 import { ActivatedRoute } from '@Angular/router';
 import { Subscription } from 'rxjs/Rx';
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './aluno-form.component.html',
   styleUrls: ['./aluno-form.component.css']
 })
-export class AlunoFormComponent implements OnInit {
+export class AlunoFormComponent implements OnInit, IFormCanDeactivate {
 
   aluno: any = {};
   inscricao: Subscription;
@@ -48,5 +49,9 @@ export class AlunoFormComponent implements OnInit {
 
     return true;
 
+  }
+
+  podeDesativar(){
+    return this.podeMudarRota();
   }
 }
